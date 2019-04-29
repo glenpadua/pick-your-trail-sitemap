24845970f631cd43aad48f34d01a9e13daf28ce2
Glen Padua <glenpadua01@gmail.com>
2019-04-29 22:44:07 +0530

    Add generic section component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import Destinations from 'components/Destinations'
-import ThemedVacations from 'components/ThemedVacations'
+import Section from 'components/Section'
 import Itineraries from 'components/Itineraries'
+import data from 'data/sample.json'
 
 import {
   Title
@@ -13,9 +13,14 @@ const App = () => {
     <div className='container xs-full-width'>
       <Title>Pickyourtrail sitemap</Title>
       {/* Destination Section - Show all destinations in alphabetical order */}
-      <Destinations />
+      <Section
+        title='Destinations'
+        data={data.destinations}
+      />
       {/* Themed Vacations Section - Show all themed vacations in alphabetical order */}
-      <ThemedVacations />
+      <Section
+        title='Themed Vacations'
+      />
       {/* Show all section -  */}
       <Itineraries />
     </div>
---
+++ src/components/Section/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SubTitle } from 'components/UI'
+
+const Section = ({ title, data }) => {
+  return (
+    <section>
+      <SubTitle>{title}</SubTitle>
+    </section>
+  )
+}
+
+export default Section
